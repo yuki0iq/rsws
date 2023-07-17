10399e101ee877c1cee7390b4840462f32d96efe
Yuki <yuki.utk8g@gmail.com>
2023-07-17 16:17:04 +0300

    move 404-html from code to separate file

--- src/main.rs
+++ src/main.rs
@@ -18,13 +18,13 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, contents) = if request_line.starts_with("GET / HTTP/") {
-        ("HTTP/1.1 200 OK", fs::read_to_string("ok.html").unwrap())
+    let (status_line, filename) = if request_line.starts_with("GET / HTTP/") {
+        ("HTTP/1.1 200 OK", "ok.html")
     } else {
-        ("HTTP/1.1 404 Not Found",
-        String::from("<!doctype html><html><head><meta charset=\"utf-8\"><title>error</title></head><body><h1>404 Not Found</h1><hr><i>rust simple web server</i></body></html>"))
+        ("HTTP/1.1 404 Not Found", "fail.html")
     };
 
+    let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
 
     let response = format!(
