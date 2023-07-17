7fb620f49e71a0eb6ed4489446911b969d58c7cb
Yuki <yuki.utk8g@gmail.com>
2023-07-17 15:45:07 +0300

    add read request

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
-use std::net::TcpListener;
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        
-        println!("Got a new commection!!");
+
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|line| line.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+    println!("Got HTTP request: {:#?}", http_request);
 }
